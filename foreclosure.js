f6bf4c750722b4cfc0fd32ebc750aa0916c672e5
Tyler Nichols <tylernchls@gmail.com>
2016-12-06 19:57:25 -1000

    added account

---
+++ foreclosure.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var steve;
+var stevesLoan;
+var month = 0;
+var monthsUntilEvicted;
+
+
+function loan(){
+  var account = {
+  borrowed: 550000,
+  balance: 286000,
+  monthlyPayment: 1700,
+  defaulted: 5,
+  foreclosed: false
+},
+
+function missPayment(){
+
+}
+}
